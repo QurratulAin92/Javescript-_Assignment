4bb61c3effb9beee9a21388d126ec581fe3e9297
QurratulAin92 <57381048+QurratulAin92@users.noreply.github.com>
2020-06-14 17:58:54 +0500

    Add files via upload

---
+++ app_3.js
@@ -0,0 +1,20 @@
+// Question 1
+var age = "I am 15 years old"
+alert(age)
+
+// Question 2
+var n = 14
+alert("You have visited this page " + n + "  times")
+
+
+// Question 3
+var birth_year = 2000
+document.write("My birth year is " + birth_year +"<br>")
+document.writeln("Data type of my declared variable is number" + "<br>" )
+
+
+// Question 4
+var vis_name = "Bilal"
+var pro_title = "Books"
+var qty = 5
+document.write(vis_name + "   ordered" + "  " +  qty + "  " + pro_title + "   on Daraz Clothing Store.")
