00daad79056d4dd451762545189b62610a91602d
QurratulAin92 <57381048+QurratulAin92@users.noreply.github.com>
2020-06-14 17:56:22 +0500

    Add files via upload

---
+++ app_1.js
@@ -0,0 +1,12 @@
+// Question 1
+alert("Hi! Welcome new user")
+
+// Question 2
+alert("Error! Please enter a valid password.")
+
+// Question 3
+alert("Welcome to JS Land...Happy Coding!")
+
+// Question 4
+alert("Welcome to JS land...")
+alert("Happy Coding! Prevent this Page from creating additional dialogs")
