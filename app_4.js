29c7df72f307e29ac68e9f68892113dfed68289f
QurratulAin92 <57381048+QurratulAin92@users.noreply.github.com>
2020-06-14 17:59:31 +0500

    Add files via upload

---
+++ app_4.js
@@ -0,0 +1,12 @@
+// Question 1
+var a = 10
+var b = 20
+var c = 30
+
+// Question 2
+levgal_var = "letter , number , undescore , dollar-sign"
+illegal_var = "1)javascript special words like alert , 2)except dollar and underscore all signs , 3)javascript reserved variable like break , 4)null , 5) true false"
+
+// Question 3
+// a
+
