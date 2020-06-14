390da4f83fedfdb9dc63a89c51cfecd48fa1dee4
QurratulAin92 <57381048+QurratulAin92@users.noreply.github.com>
2020-06-14 17:57:49 +0500

    Add files via upload

---
+++ app_2.js
@@ -0,0 +1,33 @@
+// Question 1
+var username = "Aqsa"
+
+// Question 2
+var myname = "Aqsa Muhammad Taufeeq"
+
+// Question 3
+var message = "Hello World"
+alert(message)
+
+// Question 4
+var name = "Bilal"
+var age = "16 years"
+var course = "Web and Mobile hybrid app Development"
+alert(name)
+alert(age)
+alert(course)
+
+//Question 6
+var email = "aqsataufeeq92@gmail.com";
+alert("My email address is " + email)
+
+//Question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book)
+
+//Question 8
+var a = "Yah! I can write HTML content through JavaScript"
+document.write(a)
+
+//Question 9
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design) 
